446b4f56ce0616fa0b9cff979a077dcc8993511e
Robert Kuncewicz <robert.kuncewicz@d2l.com>
2016-02-15 12:50:51 -0500

    Changing simple server to serve static files

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
 
-//Lets define a port we want to listen to
-const PORT=80; 
+app.use(express.static('demo'));
 
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
